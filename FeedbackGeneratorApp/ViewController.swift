a79edf867427092be9f02ddce9d17229aa2ca7d2
lean <sanqq5908@gmail.com>
2021-10-27 17:14:00 +0800

    Initial Commit

---
+++ FeedbackGeneratorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedbackGeneratorApp
+//
+//  Created by lean on 2021/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
